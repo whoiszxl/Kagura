01e5c77aaf08ec51e4cbddf8f0150f0b7a2192b5
zhouxiaolong <zhouxl@lamezhi.com>
2018-01-30 16:32:25 +0800

    gradle插件安装

---
+++ code/0-start-project/src/test/java/com/whoiszxl/spring/cloud/startproject/StartProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.whoiszxl.spring.cloud.startproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
